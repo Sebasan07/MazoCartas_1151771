c31c07ceea3833387655583523e812895ba93a86
Juan Sebastian Sanchez Prada <juansebastiansp.@ufps.edu.co>
2021-04-17 01:38:44 -0500

    terminado

--- js/cartas.js
+++ js/cartas.js
@@ -6,8 +6,10 @@ function cargarCartas() {
         const carta = document.createElement("div")
         const img = document.createElement("img")
 
-        carta.id = i;
-        img.className = "cartas"
+        carta.id = "btn-carta"
+        carta.className = "btn-carta"
+        carta.setAttribute("data-carta", `${i}`)
+        img.className = `btn-carta carta-${i}`
         img.src = `img/${i}.png`
 
         carta.appendChild(img)
